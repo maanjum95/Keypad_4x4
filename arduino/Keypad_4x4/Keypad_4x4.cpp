64e7c6106178752177f9412257272cf055d9ba8a
Muhammad Anjum <ma.anjum95@hotmail.com>
2015-05-11 22:12:53 +0500

    Completed the library from Arduino

---
+++ arduino/Keypad_4x4/Keypad_4x4.cpp
@@ -0,0 +1,113 @@
+/*******************************************************************************************************	
+* 	File Name: Keypad_4x4.cpp
+*	Project Name: Keypad_4x4 (Arduino)
+* 	
+* 	The MIT License (MIT)
+*
+*	Copyright (c) 2015 M.A. Anjum
+*	Email : ma.anjum95@gmail.com
+*	
+*	Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+*	and associated documentation files (the "Software"), to deal in the Software without restriction,
+*	including without limitation the rights to use, copy, modify, merge, publish, distribute,
+*	sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+*	is furnished to do so, subject to the following conditions:
+*
+*	The above copyright notice and this permission notice shall be included in all copies 
+*	or substantial portions of the Software.
+*
+*	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+*	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+*	AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+*	DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF 
+*	OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+********************************************************************************************************/
+
+#include "Keypad_4x4.h"
+
+Keypad_4x4::Keypad_4x4(byte *pins) {
+	for (int i = 0; i < PINS; i++)
+		this->pins[i] = pins[i];
+
+	// pins 0-3 should be pulled down by 1k resistor and they are to be made the output
+	pinMode(this->pins[0], OUTPUT);
+	pinMode(this->pins[1], OUTPUT);
+	pinMode(this->pins[2], OUTPUT);
+	pinMode(this->pins[3], OUTPUT);
+
+	// initializing the output to be LOW 0
+	digitalWrite(this->pins[0], 0);
+	digitalWrite(this->pins[1], 0);
+	digitalWrite(this->pins[2], 0);
+	digitalWrite(this->pins[3], 0);
+
+	// pins 4-7 should be the input and will determine the output of the keypad
+	pinMode(this->pins[4], INPUT);
+	pinMode(this->pins[5], INPUT);
+	pinMode(this->pins[6], INPUT);
+	pinMode(this->pins[7], INPUT);
+}
+
+char Keypad_4x4::readInput(void) {
+	// we loop over the output port 0-3 and set them to HIGH 1 one-by-one
+	for (int i = 0; i < 4; i++) {
+		//digitalWrite(this->pins[i], 1);
+		digitalWrite(this->pins[i], 1);
+		
+		// now we check which input pin is high from pins 4-7
+		for (int j = 4; j < 8; j++) {
+			// if the current pin is high then the corresponding button is on
+			if (digitalRead(this->pins[j])) {
+				digitalWrite(this->pins[i], 0); // output should be cleared after each loop
+				return returnButtonOutput((j + 1) * 10 + (i + 1));
+			}
+		}
+		digitalWrite(this->pins[i], 0); // output should be cleared after each loop
+	}
+
+	// if no button is pressed we return -1.
+	return -1;
+}
+
+char Keypad_4x4::returnButtonOutput(int mxn) {
+	switch (mxn) {
+		case 84:
+			return BUTTON_OUTPUT_84;
+		case 83:
+			return BUTTON_OUTPUT_83;
+		case 82:
+			return BUTTON_OUTPUT_82;
+		case 81:
+			return BUTTON_OUTPUT_81;
+		case 74:
+			return BUTTON_OUTPUT_74;
+		case 73:
+			return BUTTON_OUTPUT_73;
+		case 72:
+			return BUTTON_OUTPUT_72;
+		case 71:
+			return BUTTON_OUTPUT_71;
+		case 64:
+			return BUTTON_OUTPUT_64;
+		case 63:
+			return BUTTON_OUTPUT_63;
+		case 62:
+			return BUTTON_OUTPUT_62;
+		case 61:
+			return BUTTON_OUTPUT_61;
+		case 54:
+			return BUTTON_OUTPUT_54;
+		case 53:
+			return BUTTON_OUTPUT_53;
+		case 52:
+			return BUTTON_OUTPUT_52;
+		case 51:
+			return BUTTON_OUTPUT_51;
+		default:
+			return -1;
+	}
+}
+
+
+
+
---
+++ arduino/Keypad_4x4/Keypad_4x4.h
@@ -0,0 +1,110 @@
+/*******************************************************************************************************	
+* 	File Name: Keypad_4x4.h
+*	Project Name: Keypad_4x4 (Arduino)
+* 	
+* 	The MIT License (MIT)
+*
+*	Copyright (c) 2015 M.A. Anjum
+*	Email : ma.anjum95@gmail.com
+*	
+*	Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+*	and associated documentation files (the "Software"), to deal in the Software without restriction,
+*	including without limitation the rights to use, copy, modify, merge, publish, distribute,
+*	sublicense, and/or sell copies of the Software, and to permit persons to whom the Software 
+*	is furnished to do so, subject to the following conditions:
+*
+*	The above copyright notice and this permission notice shall be included in all copies 
+*	or substantial portions of the Software.
+*
+*	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+*	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
+*	AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+*	DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF 
+*	OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+********************************************************************************************************/
+/**
+*	4x4 Matrix Membrane Keypad (#27899)
+*						  Pins
+*		+---+---+---+---+
+*		| 1 | 2 | 3 | A |  8
+*		+---+---+---+---+
+*		| 4 | 5 | 6 | B |  7
+*		+---+---+---+---+
+*		| 7 | 8 | 9 | C |  6
+*		+---+---+---+---+
+*		| * | 0 | # | D |  5
+*		+---+---+---+---+	
+*	Pins: 4   3   2   1
+*
+*	Port : |8|7|6|5|4|3|2|1| 
+*
+*	The numbering of the pins attached via thin cable mentioned here is from right to left.
+*	So the pins are number as |8|7|6|5|4|3|2|1| when seen from the front facing the keypad upwards.
+*
+*	Working:
+*	1- The four right pins (1-4) are either pulled up or down (in this library it is pulled down by 1k).
+*	2- Then one by one the above mentioned pins are set(if pulled down) or cleared (if pulled up),
+*		and the values of the rest of the pins (5-8) are checked.
+*	3- Depending upon the state of the pins (5-8) we can check which button is pressed.
+*	
+*	** IMP ** The input ports (5-8) need to be pulled down with 1k for proper execution. As it did not work for me without pull down,
+*	even though it is not necessary for ouput ports (1-4) to be pulled down.
+*/
+
+#ifndef KEYPAD_4X4
+#define KEYPAD_4X4
+
+#include "Arduino.h"
+
+#define PINS 8
+
+// The ASCII output of each button as written on the keypad
+// The last number is mn as defined in matrix as mxn ie 84 = 8x4
+// These number dont necessarily represent the row or column number but the pin corresponding to each row or column
+#define BUTTON_OUTPUT_84 '1'
+#define BUTTON_OUTPUT_83 '2'
+#define BUTTON_OUTPUT_82 '3'
+#define BUTTON_OUTPUT_81 'A'
+
+#define BUTTON_OUTPUT_74 '4'
+#define BUTTON_OUTPUT_73 '5'
+#define BUTTON_OUTPUT_72 '6'
+#define BUTTON_OUTPUT_71 'B'
+
+#define BUTTON_OUTPUT_64 '7'
+#define BUTTON_OUTPUT_63 '8'
+#define BUTTON_OUTPUT_62 '9'
+#define BUTTON_OUTPUT_61 'C'
+
+#define BUTTON_OUTPUT_54 '*'
+#define BUTTON_OUTPUT_53 '0'
+#define BUTTON_OUTPUT_52 '#'
+#define BUTTON_OUTPUT_51 'D'
+
+class Keypad_4x4 {
+private:
+	byte pins[PINS] = {0};
+
+	/**
+	*	Returns a character depending upon the mxn combination sent to the method
+	*	The character corresponding to individual mxn is given above.
+	*	The values of mxn can be calculated from the figure given in comments.
+	*/
+	char returnButtonOutput(int mxn);
+public:
+	/**
+	*	Initializes the keypad pins from the given array of pins.
+	*	pins: An array of port number on which the keypad pins are connected.
+	*	The size of pins should be 8 (PINS) are must contain the port number in the order as defined in the beggining comments.
+	*	ie pins[7] -> port connected to pin 8 and pins[0] - > port connected to pin 1
+	*/
+	Keypad_4x4(byte *pins);
+
+	/**
+	*	Reads input from the keypad and returns the appropriate character.
+	*	In case of no input the keypad -1 is returned.
+	*/
+	char readInput(void);
+};
+
+#endif
